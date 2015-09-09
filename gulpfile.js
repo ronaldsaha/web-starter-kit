c03c8a69f410143166934c58872354bbc7ec86ba
unknown <ronaldsaha@gmail.com>
2015-09-09 11:28:56 +0600

    integrated basic typescript gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp'),
+    fs = require('fs'),
+    runSequence = require('run-sequence'),
+    del = require('del'),
     minify_css = require('gulp-minify-css'),
     rjs = require('gulp-requirejs'),
     uglify = require('gulp-uglify'),
-    replace = require('gulp-replace');
+    replace = require('gulp-replace'),
+    debug = require('gulp-debug'),
+    inject = require('gulp-inject'),
+    tsc = require('gulp-typescript'),
+    tslint = require('gulp-tslint'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('build-clean', function () {
+    //del('dist/scripts/**');
+    //del('dist/styles/**');
+});
 
 gulp.task('copy-styles-assets', function () {
     gulp.src('packages/bootstrap/dist/fonts/*.*').pipe(gulp.dest('dist/styles/fonts/bootstrap'));
@@ -17,15 +30,65 @@ gulp.task('minify-styles', function () {
         .pipe(gulp.dest('dist/styles/'));
 });
 
-gulp.task('minify-scripts', function () {
-    return rjs({
-            baseUrl: "src/scripts",
-            mainConfigFile: 'src/scripts/configuration.js',
-            name: 'main',
-            out: 'main.js',
-            preserveLicenseComments: false})
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts/'));
-})
+//gulp.task('minify-source', function () {
+//    return rjs({
+//        baseUrl: "src/scripts",
+//        mainConfigFile: 'src/scripts/configuration.js',
+//        name: 'main',
+//        out: 'main.js',
+//        preserveLicenseComments: false
+//    })
+//        .pipe(uglify())
+//        .pipe(gulp.dest('dist/scripts/'));
+//});
 
-gulp.task('build', ['copy-styles-assets', 'minify-styles', 'minify-scripts']);
+//gulp.task('default', ['copy-styles-assets', 'minify-styles', 'minify-source']);
+
+gulp.task('lint-source', function () {
+    return gulp.src('src/scripts/**/*.ts').pipe(tslint()).pipe(tslint.report('prose'));
+});
+
+gulp.task('generate-source-references', function () {
+    fs.writeFile('dist/scripts/AllSourceReferences.ts', '//{\n\n//}');
+    return gulp.src('dist/scripts/AllSourceReferences.ts')
+        .pipe(inject(gulp.src(['src/scripts/**/*.ts'], {read: false}), {
+            starttag: '//{',
+            endtag: '//}',
+            transform: function (filepath) {
+                return '/// <reference path="../..' + filepath + '" />';
+            }
+        })).pipe(gulp.dest('dist/scripts/'));
+});
+
+gulp.task('transpile-source', function () {
+    var transpiled = gulp.src(['src/scripts/**/*.ts', 'packages/typings/**/*.ts', 'dist/scripts/AllSourceReferences.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(tsc({
+            target: 'ES5',
+            declarationFiles: false,
+            noExternalResolve: true
+        }));
+
+    transpiled.dts.pipe(gulp.dest('dist/scripts/src'));
+    return transpiled.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/scripts/src'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['src/scripts/**/*.ts', 'src/styles/**/*.css'], ['copy-styles-assets', 'minify-styles', 'lint-source', 'transpile-source']);
+});
+
+
+gulp.task('default', function () {
+    runSequence(
+        'build-clean',
+        ['copy-styles-assets', 'minify-styles'],
+        'lint-source',
+        'generate-source-references',
+        'transpile-source'
+    );
+});
+
+
+
---
+++ src/scripts/application/Application.ts
@@ -0,0 +1,11 @@
+class Application {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
