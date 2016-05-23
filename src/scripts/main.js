1742eb72596e5f0251ad1e6f340498e018db8c92
ronaldsaha <ronald.saha@adpeople.com>
2016-05-23 10:30:13 +0600

    work in progress.

--- src/scripts/main.js
+++
@@ -1,4 +0,0 @@
-define(["require", "exports", 'application/application'], function (require, exports, Application) {
-    new Application('Hello World').start();
-});
-//# sourceMappingURL=main.js.map
